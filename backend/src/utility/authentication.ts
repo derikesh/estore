4632b7acb4fafe0a53c4730f478a57b9370c35dc
rikesh <rikeshsherpa1@gmail.com>
2024-11-21 09:05:57 +0545

    added middleware

--- backend/src/utility/authentication.ts
+++ backend/src/utility/authentication.ts
@@ -1,6 +1,5 @@
 import jwt from 'jsonwebtoken';
 import { Response, Request, NextFunction } from 'express';
-import { SendResponse } from 'firebase-admin/messaging';
 import { sendServerError } from './error';
 import { sendResponse } from './response';
 import dotenv from 'dotenv';
@@ -11,25 +10,26 @@ dotenv.config();
 
 const JWT_KEY = process.env.JWT_KEY;
 
+
 export interface userAuth extends Request {
     user:String | object;
 }
 
-const authMiddleware = async (req: userAuth, res: Response, next: NextFunction) => {
+const authMiddleware =  (req: userAuth, res: Response, next: NextFunction):void => {
     const token = req.header('Authorization')?.replace('Bearer: ','');
     if(!token){
-      return  sendResponse( res , 400,'authorization failed' )
+        sendResponse( res , 400,'authorization failed' );
+        return;
     }
     try {
     // verify jwt
         if(!JWT_KEY){
-            return sendResponse(res,400,'some error occured please try again later');
+            throw new Error("")
+            return;
         }
         const payload = jwt.verify( token ,JWT_KEY  ); 
         req.user = payload;
-
-        sendResponse(res,200,'user valid',{ jsonKey:payload });
-        return next();
+         next();
     } catch (err: any) {
         console.log(err.message);
         sendServerError(res,500);
